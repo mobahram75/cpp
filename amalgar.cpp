453231d224d1126eedd22c2d9dd622c6b149fd81
Mohammad Bahrampour <107625700+mobahram75@users.noreply.github.com>
2025-08-08 18:46:10 +0330

    Update amalgar.cpp

--- amalgar.cpp
+++ amalgar.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    int a , b;
+    cin >> a >> b ;
+cout <<"sum"<< a+b<< endl;
+cout << "difference"<< a-b<< endl;
+cout<< "product"<< a*b<< endl;
+cout<<"quntient"<<fixed<< setprecision(3)<< a/b<< endl;
+cout<<"remainder"<< a%b<< endl;
+return 0;
+}
